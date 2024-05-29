e21bda45a59efe88fe5ec5466bec89ffa35c61ba
HaderYeren <136634293+HaderYeren@users.noreply.github.com>
2024-05-29 19:30:22 +0300

    +

--- script.js
+++ script.js
@@ -1,22 +1,70 @@
 
-function showBeverageMessage() {
-    const beverageSelect = document.getElementById('beverage');
-    const selectedBeverage = beverageSelect.options[beverageSelect.selectedIndex].text;
-    const messageDiv = document.getElementById('beverageMessage');
-    messageDiv.textContent = `Ви обрали: ${selectedBeverage}`;
+let numbersWhile = document.getElementById("numbersWhile");
+let num = 1;
+while (num <= 10) {
+    let li = document.createElement("li");
+    li.textContent = num;
+    numbersWhile.appendChild(li);
+    num++;
 }
-function checkDay() {
-    const dayInput = document.getElementById('dayInput').value.toLowerCase();
-    const messageDiv = document.getElementById('dayMessage');
-    const weekdays = ['понеділок', 'вівторок', 'середа', 'четвер', 'пʼятниця'];
-    const weekends = ['субота', 'неділя'];
 
-    if (weekdays.includes(dayInput)) {
-        messageDiv.textContent = `${dayInput} - робочий день.`;
-    } else if (weekends.includes(dayInput)) {
-        messageDiv.textContent = `${dayInput} - вихідний день.`;
-    } else {
-        messageDiv.textContent = `Неправильний ввід або такого дня не існує.`;
+let evenNumbersFor = document.getElementById("evenNumbersFor");
+for (let i = 2; i <= 20; i += 2) {
+    let li = document.createElement("li");
+    li.textContent = i;
+    evenNumbersFor.appendChild(li);
+}
+
+let multiplicationTable = document.getElementById("multiplicationTable");
+for (let i = 1; i <= 10; i++) {
+    let li = document.createElement("li");
+    li.textContent = `7 * ${i} = ${7 * i}`;
+    multiplicationTable.appendChild(li);
+}
+
+let arrayWhile = document.getElementById("arrayWhile");
+let arr = [1, 2, 3, 4, 5];
+let index = 0;
+while (index < arr.length) {
+    let li = document.createElement("li");
+    li.textContent = arr[index];
+    arrayWhile.appendChild(li);
+    index++;
+}
+
+let excludeSeven = document.getElementById("excludeSeven");
+let arrExcludeSeven = Array.from({ length: 10 }, (_, i) => i + 1);
+for (let num of arrExcludeSeven) {
+    if (num === 7) {
+        break;
+    }
+    let li = document.createElement("li");
+    li.textContent = num;
+    excludeSeven.appendChild(li);
+}
+
+let lessThanN = document.getElementById("lessThanN");
+let n = 15;
+let num = 1;
+while (num < n) {
+    let li = document.createElement("li");
+    li.textContent = num;
+    lessThanN.appendChild(li);
+    num++;
+    if (num >= n) {
+        break;
     }
 }
-document.getElementById('beverage').addEventListener('change', showBeverageMessage);
+
+let notMultipleOfThree = document.getElementById("notMultipleOfThree");
+num = 1;
+while (num <= 20) {
+    if (num % 3 === 0) {
+        num++;
+        continue;
+    }
+    let li = document.createElement("li");
+    li.textContent = num;
+    notMultipleOfThree.appendChild(li);
+    num++;
+}
